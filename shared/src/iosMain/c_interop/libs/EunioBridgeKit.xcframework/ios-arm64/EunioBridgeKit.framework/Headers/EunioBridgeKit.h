846c0555625c5a49292bd2f8f4c0a3ff255314b2
Ondřej Šesták <support@moveconcept.cz>
2025-10-30 11:52:16 +0100

    chore: add framework and re-enable iOS

---
+++ shared/src/iosMain/c_interop/libs/EunioBridgeKit.xcframework/ios-arm64/EunioBridgeKit.framework/Headers/EunioBridgeKit.h
@@ -0,0 +1,17 @@
+//
+//  EunioBridgeKit.h
+//  EunioBridgeKit
+//
+//  Umbrella header for Kotlin/Native cinterop
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EunioBridgeKit.
+FOUNDATION_EXPORT double EunioBridgeKitVersionNumber;
+
+//! Project version string for EunioBridgeKit.
+FOUNDATION_EXPORT const unsigned char EunioBridgeKitVersionString[];
+
+// Public headers
+#import <EunioBridgeKit/FirebaseBridge.h>
